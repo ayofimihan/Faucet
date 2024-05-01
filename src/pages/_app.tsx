0d08893112eb4b6e2ced0fb473b05ef2b7952c6d
ayofimihan <cogunsina@gmail.com>
2024-05-01 18:43:33 +0100

    fix metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,6 +33,8 @@ export default function App({ Component, pageProps }: AppProps) {
                 name="description"
                 content="Get Sepolia and Base Sepolia tokens from the faucet"
               />
+              <meta name="author" content="champion" />
+              <link rel="author" href="https://0x13.dev" />
               <meta
                 name="image"
                 content="https://res.cloudinary.com/godfimihan/image/upload/v1712948328/Base_Sepola_elloin.jpg"
@@ -42,7 +44,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 content="sepolia, base sepolia, faucet, test tokens, base network, eth, test eth, free eth, testnet, testnet tokens"
               />
               <meta name="application-name" content="Base Sepolia Faucet" />
-              <meta name="author" content="https://github.com/ayofimihan" />
+              <meta name="creator" content="champion" />
               <meta property="og:title" content="Base Sepolia Faucet" />
               <meta
                 property="og:description"
@@ -52,7 +54,26 @@ export default function App({ Component, pageProps }: AppProps) {
                 property="og:url"
                 content="https://base-sepolia-faucet.vercel.app"
               />
+              <meta property="og:site_name" content="Base Sepolia Faucet" />
+              <meta property="og:locale" content="en_US" />
+              <meta
+                property="og:image "
+                content="https://res.cloudinary.com/godfimihan/image/upload/v1712948328/Base_Sepola_elloin.jpg"
+              />
+              <meta property="og:image:width" content="1200" />
+              <meta property="og:image:height" content="630" />
+              <meta property="og:image:alt" content="Base Sepolia Faucet" />
               <meta property="og:type" content="website" />+{" "}
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta name="twitter:creator" content="@F1m1han" />
+              <meta
+                name="twitter:description"
+                content="Get Base Sepolia and Sepolia tokens from the faucet"
+              />
+              <meta
+                name="twitter:image"
+                content="https://res.cloudinary.com/godfimihan/image/upload/v1712948328/Base_Sepola_elloin.jpg"
+              ></meta>
             </Head>
             <Component {...pageProps} />
           </RootLayout>
